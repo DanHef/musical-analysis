84ca1176d81ff586cdd2aeb13d01bdc97f287c40
DanHef <daniel.heffter@sap.com>
2020-06-17 08:10:02 +0200

    Fixed Environment Issue

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import WaveSurfer from 'wavesurfer.js';
-import { environment } from '../../environments/environment';
+import { environment } from '../environments/environment';
 
 @Component({
   selector: 'app-root',
